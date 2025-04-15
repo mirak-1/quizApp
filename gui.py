d939000de9d63dea8ae4f7df81e165c704a7249d
p2414115 <bencheikh.abd.karim@gmail.com>
2025-04-15 10:36:53 +0200

    add skip button + fix of the congratulation layout (now clears the layout before displaying the new one

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QRadioButton, QHBoxLayout, QPushButton,
-                             QVBoxLayout, QLabel, QButtonGroup)
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout, QPushButton,
+                             QVBoxLayout, QLabel)
 from PyQt5.QtCore import Qt, QTimer
 import quiz
 class QuizApp(QWidget):
@@ -19,10 +19,12 @@ class QuizApp(QWidget):
         self.question_label.setStyleSheet("font-size: 18px; padding: 10px;")
         self.question_label.setWordWrap(True)
         self.question_label.setAlignment(Qt.AlignCenter)
+        self.skip = QPushButton("Skip question", self)
         
+        self.skip.clicked.connect(lambda checked: self.load_next_question())
         # Add Label to the layout
         layout.addWidget(self.question_label)
-        
+        layout.addWidget(self.skip)
         # Layout containing the answers
         self.answers_group = []
         self.answers = QVBoxLayout()
@@ -62,14 +64,37 @@ class QuizApp(QWidget):
                 row1.addWidget(btn)
                 
             self.answers.addLayout(row1)
+    
+    def load_end_screen(self):
+        self.congratulation_label = QLabel("Congratulations, you finished the quiz! ")
+        self.congratulation_label.setStyleSheet("font-size: 18px; padding: 10px;")
+        self.congratulation_label.setAlignment(Qt.AlignCenter)
+        
+        self.clear_layout()
+        self.layout().addWidget(self.congratulation_label)
+        
+    
+    def clear_layout(self):
+        if self.layout():
+            self.clear_answers()
+            while self.layout().count():
+                item = self.layout().takeAt(0)
+                if item.widget():
+                    item.widget().deleteLater()
+                elif item.layout():
+                    while item.layout().count():
+                        child = item.layout().takeAt(0)
+                        if child.layout():
+                            child.widget().deleteLater()
             
+        
+         
     def check_answer(self, text, correct, b: QPushButton):
         for btn in self.answers_group:
             btn.setEnabled(False)
         if text == correct:
             b.setStyleSheet("border: 1px solid #00FF00; background-color: white; border-radius: 15px; color: #00FF00")
         else:
-            print("Wrong answer! ")
             b.setStyleSheet("border: 1px solid red; background-color: white; border-radius: 15px; color: red")
         
         QTimer.singleShot(2000, self.load_next_question)
@@ -79,13 +104,12 @@ class QuizApp(QWidget):
             q = self.questions[index]
             self.update_layout(q['question'], q['answers'], q['correct'])
         else:
-            self.clear_answers()
-            self.question_label.setText("The quiz is now completed")
+            self.load_end_screen()
 
     def load_next_question(self):
         self.current_question += 1
         self.load_question(self.current_question)
-        
+    
     def clear_answers(self):
          while self.answers.count():
             child = self.answers.takeAt(0)
